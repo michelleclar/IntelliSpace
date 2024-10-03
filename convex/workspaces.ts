4af321a2b49e2c42847f2eb416666a8222005527
Carl <carlmichelle493@gmail.com>
2024-10-03 23:16:44 +0800

    add convex

---
+++ convex/workspaces.ts
@@ -0,0 +1,41 @@
+import { v } from "convex/values";
+import { mutation, query } from "./_generated/server";
+import { getAuthUserId } from "@convex-dev/auth/server";
+import { Fields } from "../fields";
+
+export const create = mutation({
+  args: { name: v.string() },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+    if (!userId) {
+      throw new Error(Fields.UNAUTHORIZED);
+    }
+    const joinCode = "123456";
+    const workspaceId = await ctx.db.insert("workspaces", {
+      name: args.name,
+      userId,
+      joinCode,
+    });
+    // const workspace = await ctx.db.get(workspaceId);
+    return workspaceId;
+  },
+});
+
+export const get = query({
+  args: {},
+  handler: async (ctx) => {
+    return await ctx.db.query("workspaces").collect();
+  },
+});
+
+export const getById = query({
+  args: { id: v.id("workspaces") },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+    if (!userId) {
+      throw new Error(Fields.UNAUTHORIZED);
+    }
+
+    return await ctx.db.get(args.id);
+  },
+});
---
+++ src/middleware.ts
@@ -0,0 +1,26 @@
+// TODO: 拦截规则
+import {
+  convexAuthNextjsMiddleware,
+  createRouteMatcher,
+  nextjsMiddlewareRedirect,
+} from "@convex-dev/auth/nextjs/server";
+
+const isSignInPage = createRouteMatcher(["/auth"]);
+const isProtectedRoute = createRouteMatcher(["/product(.*)"]);
+
+export default convexAuthNextjsMiddleware((request, { convexAuth }) => {
+  if (!isSignInPage(request) && !convexAuth.isAuthenticated()) {
+    return nextjsMiddlewareRedirect(request, "/auth");
+  }
+  if (isSignInPage(request) && convexAuth.isAuthenticated()) {
+    return nextjsMiddlewareRedirect(request, "/");
+  }
+  // if (isProtectedRoute(request) && !convexAuth.isAuthenticated()) {
+  //   return nextjsMiddlewareRedirect(request, "/signin");
+  // }
+});
+export const config = {
+  // The following matcher runs middleware on all routes
+  // except static assets.
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
