d74dbbc8ba769e70509abc9be1a0dfe345fbec2d
Carl <carlmichelle493@gmail.com>
2024-10-05 23:50:47 +0800

    add schema (member,channels)

---
+++ convex/channles.ts
@@ -0,0 +1,52 @@
+import {getAuthUserId} from "@convex-dev/auth/server";
+import {mutation, query} from "./_generated/server";
+import {v} from "convex/values";
+import {Fields} from "../fields";
+
+export const get = query({
+  args: { workspaceId: v.id("workspaces") },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+    if (!userId) {
+      return [];
+    }
+    const member = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", args.workspaceId).eq("userId", userId),
+      )
+      .unique();
+    if (!member) {
+      return [];
+    }
+    return await ctx.db
+        .query("channels")
+        .withIndex("by_workspace_id", (q) =>
+            q.eq("workspaceId", args.workspaceId),
+        )
+        .collect();
+  },
+});
+
+export const create = mutation({
+  args: { name: v.string(), workspaceId: v.id("workspaces") },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+    if (!userId) throw new Error(Fields.UNAUTHORIZED);
+
+    const member = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", args.workspaceId).eq("userId", userId),
+      )
+      .unique();
+    if (!member || member.role !== "admin")
+      throw new Error(Fields.UNAUTHORIZED);
+    const parsedName = args.name.replace(/\s+/g, "-").toLowerCase();
+    // const workspace = await ctx.db.get(workspaceId);
+    return await ctx.db.insert("channels", {
+      name: parsedName,
+      workspaceId: args.workspaceId,
+    });
+  },
+});
