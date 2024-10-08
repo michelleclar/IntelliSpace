392829a733943fddc19f6ad8491b2a9f50205e13
Carl <carlmichelle493@gmail.com>
2024-10-08 21:52:43 +0800

    message get api

--- src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -3,6 +3,7 @@
 import { ChatInput } from "./chat-input";
 import { Header } from "./header";
 import { useGetChannel } from "@/features/channels/api/use-get-channle";
+import { useGetMessages } from "@/features/messages/api/use-get-messages";
 import { useChannelId } from "@/hooks/use-channel-id";
 import { Loader, TriangleAlert } from "lucide-react";
 
@@ -11,6 +12,9 @@ const ChannelIdPage = () => {
   const { channel, isLoading: channelIsLoading } = useGetChannel({
     id: channelId,
   });
+
+  const { results } = useGetMessages({ channelId });
+  console.log({ results });
   if (channelIsLoading) {
     return (
       <div className="h-full flex-1 flex items-center justify-center">
---
+++ src/features/messages/api/use-get-messages.ts
@@ -0,0 +1,42 @@
+import { usePaginatedQuery } from "convex/react";
+import { api } from "../../../../convex/_generated/api";
+import { Id } from "../../../../convex/_generated/dataModel";
+import { useState } from "react";
+import { type } from "os";
+
+const BATCH_SIZE = 20;
+type Options = {
+  onSuccess?: (data: ResponseType) => void;
+  onError?: (error: Error) => void;
+  onSettled?: () => void;
+  throwError?: boolean;
+};
+
+interface UseGetMessagesProps {
+  channelId?: Id<"channels">;
+  conversationId?: Id<"conversations">;
+  parentMessageId?: Id<"messages">;
+}
+export type GetMessagesReturnType =
+  (typeof api.messages.get._returnType)["page"];
+
+export const useGetMessages = ({
+  channelId,
+  conversationId,
+  parentMessageId,
+}: UseGetMessagesProps) => {
+  const { results, status, loadMore } = usePaginatedQuery(
+    api.messages.get,
+    {
+      channelId,
+      parentMessageId,
+      conversationId,
+    },
+    { initialNumItems: BATCH_SIZE },
+  );
+  return {
+    results,
+    status,
+    loadMore: () => loadMore(BATCH_SIZE),
+  };
+};
