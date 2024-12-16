aba0f8132838ea9f856276ec8c2b27b2c1834682
carl <carlmichelle493@gmail.com>
2024-12-16 19:36:15 +0800

    Update canvas.tsx

--- src/components/canvas.tsx
+++ src/components/canvas.tsx
@@ -106,7 +106,7 @@ const Canvas = ({ defaultValue, innerRef, id }: CanvasProps) => {
         if (!elements || !elements.length) {
           return;
         }
-        appState["collaborators"] = [];
+        appState.delete("collaborators");
         const value = { elements, appState, files };
         localStorage.setItem(id, JSON.stringify(value));
       }}
