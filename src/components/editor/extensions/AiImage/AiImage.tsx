11ccf463ace5708d222d5e8de692c864882bae84
carl <carlmichelle493@gmail.com>
2024-12-29 15:12:31 +0800

    add document-item.tsx

--- src/components/editor/extensions/AiImage/AiImage.tsx
+++ src/components/editor/extensions/AiImage/AiImage.tsx
@@ -1,6 +1,7 @@
 import { mergeAttributes, Node } from '@tiptap/core'
 import { ReactNodeViewRenderer } from '@tiptap/react'
-// @ts-ignore
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
 import { v4 as uuid } from 'uuid'
 
 import { AiImageView } from './components/AiImageView'
