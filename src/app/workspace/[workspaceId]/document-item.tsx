abf1bda1ff86a857d53948d24aca8168225b1408
carl <carlmichelle493@gmail.com>
2024-12-30 11:21:41 +0800

    design document list components

--- src/app/workspace/[workspaceId]/document-item.tsx
+++ src/app/workspace/[workspaceId]/document-item.tsx
@@ -255,7 +255,7 @@ const Item = ({
           </DropdownMenu>
           <div
             role="button"
-            onClick={() => setDocumentOpen(createModelFlag)}
+            onClick={() => setDocumentOpen(!createModelFlag)}
             className="opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600"
           >
             <Plus className="h-4 w-4 text-muted-foreground" />
