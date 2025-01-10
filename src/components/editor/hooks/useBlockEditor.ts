40a9719b9c0fb6434fa124577a7e75096320c24e
carl <carlmichelle493@gmail.com>
2025-01-10 22:25:13 +0800

    fix document list components title

--- src/components/editor/hooks/useBlockEditor.ts
+++ src/components/editor/hooks/useBlockEditor.ts
@@ -21,123 +21,125 @@ import { AiImage, AiWriter } from "@/components/editor/extensions";
 // }
 
 export const useBlockEditor = ({
-  aiToken,
-  ydoc,
-  provider,
-  userId,
-  userName = "Maxi",
-  initialContent,
-  onUpdate,
-}: {
-  aiToken?: string;
-  ydoc: YDoc | null;
-  provider?: TiptapCollabProvider | null | undefined;
-  userId?: string;
-  userName?: string;
-  initialContent?: string;
-  onUpdate: (content: string) => void;
+                                   aiToken,
+                                   ydoc,
+                                   provider,
+                                   userId,
+                                   userName = "Maxi",
+                                   initialContent,
+                                   onUpdate,
+                               }: {
+    aiToken?: string;
+    ydoc: YDoc | null;
+    provider?: TiptapCollabProvider | null | undefined;
+    userId?: string;
+    userName?: string;
+    initialContent?: string;
+    onUpdate: (content: string) => void;
 }) => {
-  const [collabState, setCollabState] = useState<WebSocketStatus>(
-    provider ? WebSocketStatus.Connecting : WebSocketStatus.Disconnected,
-  );
+    const [collabState, setCollabState] = useState<WebSocketStatus>(
+        provider ? WebSocketStatus.Connecting : WebSocketStatus.Disconnected,
+    );
 
-  const editor = useEditor(
-    {
-      immediatelyRender: true,
-      shouldRerenderOnTransaction: false,
-      autofocus: true,
-      onUpdate: (ctx) => {
-        console.log(ctx.editor.getHTML());
-        onUpdate(ctx.editor.getHTML());
-      },
-      onCreate: (ctx) => {
-        // TODO: step1 need add default template
-        // TODO: step2 choice template
-        ctx.editor.commands.setContent(!!initialContent ? initialContent : "");
-        // cooperation
-        // if (provider && !provider.isSynced) {
-        //   provider.on("synced", () => {
-        //     setTimeout(() => {
-        //       if (ctx.editor.isEmpty) {
-        //         ctx.editor.commands.setContent(initialContent);
-        //       }
-        //     }, 0);
-        //   });
-        // } else if (ctx.editor.isEmpty) {
-        //   ctx.editor.commands.setContent(initialContent);
-        //   ctx.editor.commands.focus("start", { scrollIntoView: true });
-        // }
-      },
-      extensions: [
-        ...ExtensionKit({
-          provider,
-        }),
-        provider && ydoc
-          ? Collaboration.configure({
-              document: ydoc,
-            })
-          : undefined,
-        provider
-          ? CollaborationCursor.configure({
-              provider,
-              user: {
-                name: randomElement(userNames),
-                color: randomElement(userColors),
-              },
-            })
-          : undefined,
-        aiToken
-          ? AiWriter.configure({
-              authorId: userId,
-              authorName: userName,
-            })
-          : undefined,
-        aiToken
-          ? AiImage.configure({
-              authorId: userId,
-              authorName: userName,
-            })
-          : undefined,
-        aiToken ? Ai.configure({ token: aiToken }) : undefined,
-      ].filter((e): e is AnyExtension => e !== undefined),
-      editorProps: {
-        attributes: {
-          autocomplete: "off",
-          autocorrect: "off",
-          autocapitalize: "off",
-          class: "min-h-full",
+    const editor = useEditor(
+        {
+            immediatelyRender: true,
+            shouldRerenderOnTransaction: false,
+            autofocus: true,
+            onUpdate: (ctx) => {
+                console.log(ctx.editor.getHTML());
+                if (ctx.editor.getText().trim().length !== 0)
+                    onUpdate(ctx.editor.getHTML());
+
+            },
+            onCreate: (ctx) => {
+                // TODO: step1 need add default template
+                // TODO: step2 choice template
+                ctx.editor.commands.setContent(!!initialContent ? initialContent : "");
+                // cooperation
+                // if (provider && !provider.isSynced) {
+                //   provider.on("synced", () => {
+                //     setTimeout(() => {
+                //       if (ctx.editor.isEmpty) {
+                //         ctx.editor.commands.setContent(initialContent);
+                //       }
+                //     }, 0);
+                //   });
+                // } else if (ctx.editor.isEmpty) {
+                //   ctx.editor.commands.setContent(initialContent);
+                //   ctx.editor.commands.focus("start", { scrollIntoView: true });
+                // }
+            },
+            extensions: [
+                ...ExtensionKit({
+                    provider,
+                }),
+                provider && ydoc
+                    ? Collaboration.configure({
+                        document: ydoc,
+                    })
+                    : undefined,
+                provider
+                    ? CollaborationCursor.configure({
+                        provider,
+                        user: {
+                            name: randomElement(userNames),
+                            color: randomElement(userColors),
+                        },
+                    })
+                    : undefined,
+                aiToken
+                    ? AiWriter.configure({
+                        authorId: userId,
+                        authorName: userName,
+                    })
+                    : undefined,
+                aiToken
+                    ? AiImage.configure({
+                        authorId: userId,
+                        authorName: userName,
+                    })
+                    : undefined,
+                aiToken ? Ai.configure({token: aiToken}) : undefined,
+            ].filter((e): e is AnyExtension => e !== undefined),
+            editorProps: {
+                attributes: {
+                    autocomplete: "off",
+                    autocorrect: "off",
+                    autocapitalize: "off",
+                    class: "min-h-full",
+                },
+            },
         },
-      },
-    },
-    [ydoc, provider],
-  );
-  const users = useEditorState({
-    editor,
-    selector: (ctx): (EditorUser & { initials: string })[] => {
-      if (!ctx.editor?.storage.collaborationCursor?.users) {
-        return [];
-      }
+        [ydoc, provider],
+    );
+    const users = useEditorState({
+        editor,
+        selector: (ctx): (EditorUser & { initials: string })[] => {
+            if (!ctx.editor?.storage.collaborationCursor?.users) {
+                return [];
+            }
 
-      return ctx.editor.storage.collaborationCursor.users.map(
-        (user: EditorUser) => {
-          const names = user.name?.split(" ");
-          const firstName = names?.[0];
-          const lastName = names?.[names.length - 1];
-          const initials = `${firstName?.[0] || "?"}${lastName?.[0] || "?"}`;
+            return ctx.editor.storage.collaborationCursor.users.map(
+                (user: EditorUser) => {
+                    const names = user.name?.split(" ");
+                    const firstName = names?.[0];
+                    const lastName = names?.[names.length - 1];
+                    const initials = `${firstName?.[0] || "?"}${lastName?.[0] || "?"}`;
 
-          return { ...user, initials: initials.length ? initials : "?" };
+                    return {...user, initials: initials.length ? initials : "?"};
+                },
+            );
         },
-      );
-    },
-  });
-
-  useEffect(() => {
-    provider?.on("status", (event: { status: WebSocketStatus }) => {
-      setCollabState(event.status);
     });
-  }, [provider]);
 
-  // window.editor = editor;
+    useEffect(() => {
+        provider?.on("status", (event: { status: WebSocketStatus }) => {
+            setCollabState(event.status);
+        });
+    }, [provider]);
 
-  return { editor, users, collabState };
+    // window.editor = editor;
+
+    return {editor, users, collabState};
 };
